b1a3fc330807f56398835e4e927e27350a00f58c
Mohammed Kaludi <mohammed@magazine3.com>
2017-01-03 20:23:54 +0530

    Initial commit
    Initial commit
    - Plugin Header added

---
+++ ampforwp-custom-theme.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: AMP Custom Theme
+Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
+Description: Custom Theme made for AMP for WP.
+Version: 1.0
+Author:  Mohammed Kaludi
+Author URI: http://ampforwp.com/ 
+License: GPL2
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
